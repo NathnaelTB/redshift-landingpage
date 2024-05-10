c20e0753ed0265b203a67b634d20fdb26bd6f674
NathnaelTB <natibekaffa12@gmail.com>
2024-05-10 21:26:32 +0300

    add: card data, modification: banner title

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import './App.css';
-import Banner from './components/Banner';
-import Card from './components/Card';
-import Button from './components/Button';
-import Footer from './components/Footer';
-
-
+import React from "react";
+import NavBar from "./components/NavBar";
+import "./App.css";
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Button from "./components/Button";
+import Footer from "./components/Footer";
+import dummyData from "./assets/data.json";
 
 const App = () => {
-  return(
+  return (
     <div>
       <NavBar />
       <Banner />
       <h1>REDSHIFT SERVICES</h1>
       <div className="decoration-line"></div>
       <div className="card-layout">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {dummyData["services"].map((content) => (
+          <Card title={content.title} paragraph={content.paragraph} />
+        ))}
       </div>
       <div className="btn-2">
-        <Button title="read more"/>
+        <Button title="read more" />
       </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="card">
-            <h3>Hybrid vehicle Manufacturing</h3>
-            <p>
-                As a forward-thinking manufacturer, Redshift is at the forefront of
-                producing eco-friendly electric and hybrid vehicles. Our state-of-the-art 
-                Manufacturing plant, operational since 2018, is dedicated to crafting 
-                tricycles and other modern mobility solutions with a focus on sustainability.
-            </p>
+            <h3>{props.title}</h3>
+            <p>{props.paragraph}</p>
         </div>
     );
 }
